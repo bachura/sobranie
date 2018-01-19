6a6a04ba16e24cc2b3b355969595c8c470f61430
Alexey Bachurin <bachura95@gmail.com>
2018-01-19 02:32:53 +0200

    first page v1

--- js/main.js
+++ js/main.js
@@ -48,6 +48,7 @@ $(document).ready(function() {
 		if($(this).children(".video").get(0).paused){
 			$(this).children(".video").get(0).play();
 			$(this).children(".playpause").fadeOut();
+			// $(this).children(".video").css("border", "none")
 		}else{
 			$(this).children(".video").get(0).pause();
 			$(this).children(".playpause").fadeIn();
